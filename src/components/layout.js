ba0afe57725e972d5e312dd2e673b45eece15b30
ChabaJosa <chabagjg@gmail.com>
2020-07-31 14:52:43 -0400

    Tried to add dynamic navbar from semantic ui but it did

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,8 +9,10 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+// import Header from "./header"
+import Menu from "./menu"
 import "./layout.css"
+import "semantic-ui-css/semantic.min.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,16 +27,11 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+      <div>
+        <Menu />
         <main>{children}</main>
-        <footer>
+        <footer style={{ position: "absolute", bottom: "3vh", right: "3vw" }}>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,30 +1,42 @@
-import React from "react"
-import { nominalTypeHack } from "prop-types"
-import {Link } from "react-router-dom"
+import React,{useState} from "react"
+import Link from "gatsby-link"
+
+import { Menu as SemanticMenu, Segment } from "semantic-ui-react"
 
 export default function Menu() {
 
-    // Left it at: https://youtu.be/6YhqQ2ZW1sc?t=1153
-  return (
-    <div
-      style={{
-        background: "#f4f4f4",
-        paddingTop: "10px",
-      }}
-    >
-      <ul
-        style={{
-          listStyle: nominalTypeHack,
-          display: "flex",
-          justifyContent: "space-evenly",
-        }}
-      >
-          <li><Link to="/">Home</Link> </li>
-          <li><Link to="/about">About</Link> </li>
-          <li><Link to="/services">Services</Link> </li>
-          <li><Link to="/">Home</Link> </li>
+  const pathname = window.location.pathname; // Returns current route
+  const path = pathname === "/" ? "/" : pathname.substr(1); // Gets a string from the pathname after the backlash
+  const { activeItem, setActiveItem } = useState(path)
+  const handleItemClick = (e, { name }) => setActiveItem(name);
 
-      </ul>
-    </div>
+  return (
+    <Segment inverted>
+      <SemanticMenu inverted secondary>
+        
+        <SemanticMenu.Item
+          as={Link}
+          to="/"
+          name="Home"
+          active={activeItem === "/"}
+          onClick={handleItemClick}
+        />
+        <SemanticMenu.Item
+          as={Link}
+          to="/about"
+          name="About"
+          active={activeItem === "About"}
+          onClick={handleItemClick}
+        />
+        <SemanticMenu.Item
+          as={Link}
+          to="/services"
+          name="Services"
+          active={activeItem === "Services"}
+          onClick={handleItemClick}
+        />
+      </SemanticMenu>
+    </Segment>
   )
+
 }
